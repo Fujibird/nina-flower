a087659dc806aa8b0b6c3ae4616e59836e3161fd
Fujibird <johndeniel0@gmail.com>
2025-06-27 13:15:28 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Happy birthday my babi, enjoy your day! I love youu <33').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
